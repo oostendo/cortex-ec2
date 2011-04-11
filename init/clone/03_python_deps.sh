1741e9fee1ce26fcdab8a710fd3b028d3d65f79d
oostendo <nate@oostendorp.net>
2011-04-11 13:34:19 -0400

    fix path symlink to SimpleCV

--- init/clone/03_python_deps.sh
+++ init/clone/03_python_deps.sh
@@ -1,5 +1,5 @@
 cd src
-ln -s SimpleCV/SimpleCV.py cortex/lib/python2.6/SimpleCV.py
+ln -s ../../../simplecv/SimpleCV.py cortex/lib/python2.6/SimpleCV.py
 cd cortex
 source bin/activate
 easy_install redis
