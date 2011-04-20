1a7ff09f3688ebb97dc3cc5b69f9520e38da5824
oostendo <nate@oostendorp.net>
2011-04-20 13:32:40 -0400

    add cvblob-python to the stack

--- init/clone/03_python_deps.sh
+++ init/clone/03_python_deps.sh
@@ -1,5 +1,6 @@
 cd src
 ln -s ../../../simplecv/SimpleCV.py cortex/lib/python2.6/SimpleCV.py
+cp -r cvblob/contrib/cvblob-python/cvblob cortex/lib/python2.6/
 cd cortex
 source bin/activate
 easy_install redis
