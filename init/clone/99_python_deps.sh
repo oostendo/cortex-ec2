059ed2e9ff3325ed643c1f28f314624de528f2a7
Nate Oostendorp <nate@oostendorp.net>
2011-04-21 15:46:25 +0000

    add zxing-python to the cortex python environment

--- init/clone/99_python_deps.sh
+++ init/clone/99_python_deps.sh
@@ -1,6 +1,7 @@
 cd src
 ln -s ../../../simplecv/SimpleCV.py cortex/lib/python2.6/SimpleCV.py
 cp -r cvblob/contrib/cvblob-python/cvblob cortex/lib/python2.6/
+cp -r zxing-1.6/python-zxing/zxing.py cortex/lib/python2.6/
 cd cortex
 source bin/activate
 easy_install redis
